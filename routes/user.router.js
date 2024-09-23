f91a49518de13c44b29005322565cd5e78f0d7d5
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-23 16:43:06 +0500

    JWT admin route response

--- routes/user.router.js
+++ routes/user.router.js
@@ -10,7 +10,7 @@ const { adminAuth, userAuth } = require("../middlewares/auth.js");
 router.post("/rigesteruser", createnewuser);
 router.get("/verify-email", verifyEmail);
 router.post('/login', login);
-router.get('/admin', adminAuth)
+router.get('/admin', adminAuth, (req, res) => res.send('Admin Route'))
 router.get('/basic', userAuth, (req, res) => res.send('User Route'))
 
 module.exports = router;
