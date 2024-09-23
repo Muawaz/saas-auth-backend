93cad79561761480c65991aa73cf69304f0ea52e
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-23 12:40:48 +0500

    JWT authentication

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const port = process.env.PORT;
 const { connectDB } = require("./dbconnection/connection.js");
 const compagin = require("./routes/compagin.router.js");
 const user = require("./routes/user.router.js");
+const { userAuth, adminAuth } = require("./middlewares/auth.js");
+const cookieParser = require('cookie-parser')
 require("./models/associations.js");
 app.use(express.json());
 
 // Call the connectDB function to test the connection
 connectDB();
+app.use(cookieParser());
 
 app.use("/api/compagin", compagin);
 app.use("/api/user", user);
